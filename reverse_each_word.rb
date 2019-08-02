1f69cd583349ae5586e66a0b7c9eb7c73a22da7a
pragmatic-memory-1672 <hp.mars.le@gmail.com>
2019-08-02 02:28:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
 def reverse_each_word(sentence)
+ #reversing each word in the string using .collect enumerable
   reversed_array = sentence.split.collect{|word| word.reverse}
-  changed_to_array = sentence.split(' ')
   
+  #reversing each word in the string using .each enumerable
+  #.each returns the original array, so you need to append each
+  # reversed word to a new array and not count on the original array that .each returns to 
+  changed_to_array = sentence.split(' ')
   reversed_array = []
   i = 0 
   changed_to_array.each do |word| 
@@ -9,8 +13,6 @@ def reverse_each_word(sentence)
     reversed_array << rword
   end
   reversed_array.join(' ')
-  # reversed_sentence = reversed_array.join(' ')
-  # reversed_sentence
   
   # i = 0 
   # reversed_array = []
